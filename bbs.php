48a872e9926b8a69be8f398edf7290e4965b2aa7
koi-2000 <113817139+koi-2000@users.noreply.github.com>
2022-11-02 14:06:41 +0900

    Update bbs.php

--- bbs.php
+++ bbs.php
@@ -10,3 +10,13 @@ fputs($fp, sprintf("<b>%s</b> 「%s」<br />", $_POST["name"], $_POST["message"]
 fclose($fp);
 
 ?>
+<HTML> 
+    <HEAD> 
+        <TITLE>ジャンプ</TITLE> 
+        <META http-equiv="Refresh" content="0.1;URL=comment.php"> 
+            </HEAD> 
+        <BODY> 
+            <P>1秒後にジャンプします</P> 
+            <?php $name = $_POST{'name'}; $txt = $_POST{'txt'}; $txt = htmlspecialchars($txt); if(isset($txt)) { $file = file_get_contents('log.html'); $fp = fopen('log.html', 'w'); fputs($fp, $name . "<br />" . $txt . "<hr>" . "\n" . $file); fclose($fp); } ?> 
+        </BODY> 
+        </HTML> 
